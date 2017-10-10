d5ec2062dfd6f4ea06a509b01d11eaac7ee3c466
Waamber <amberastuart@aol.com>
2017-10-09 20:57:33 -0500

    added promises

--- dist/app.js
+++ dist/app.js
@@ -3,8 +3,81 @@
 const dom = require('./dom');
 let dinosaurs = [];
 
+
+//OLD WAY OF MULTIPLE JSON GETTER
+// const dinoGetter = () => {
+//   $.ajax('./data/dinosaurs.json').done((data1) => {
+//     data1.dinosaurs1.forEach((dino) => { //loops through dinoData and puts each dino object in array into dinosaurs array at the top
+//       dinosaurs.push(dino);
+//     });
+//   });
+//   $.ajax('./data/dinosaurs2.json').done((data2) => {
+//     data2.dinosaurs2.forEach((dino) => {
+//       dinosaurs.push(dino);
+//     });
+//   });
+//   $.ajax('./data/dinosaurs3.json').done((data3) => {
+//     data3.dinosaurs3.forEach((dino) => {
+//       dinosaurs.push(dino);
+//     });
+//     console.log(dinosaurs);
+//   });
+// };
+
 const initializer = () => {
-  dom.domString({ name: 'T-Rex' });
+  // dom.domString({ name: 'T-Rex' });
+  dinoGetter();
+};
+
+const firstDinosaurJSON = () => {
+  return new Promise((resolve, reject) => {
+    $.ajax('./data/dinosaurs.json').done((data) => {
+      resolve(data.dinosaurs1);
+    }).fail((error) => {
+      reject(error);
+    });
+  });
+};
+
+const secondDinosaurJSON = () => {
+  return new Promise((resolve, reject) => {
+    $.ajax('./data/dinosaurs2.json').done((data) => {
+      resolve(data.dinosaurs2);
+    }).fail((error) => {
+      reject(error);
+    });
+  });
+};
+
+const thirdDinosaurJSON = () => {
+  return new Promise((resolve, reject) => {
+    $.ajax('./data/dinosaurs3.json').done((data) => {
+      resolve(data.dinosaurs3);
+    }).fail((error) => {
+      reject(error);
+    });
+  });
+};
+
+const dinoGetter = () => {
+  firstDinosaurJSON().then((results) => {//.then()-happens when resolved // .catch()-happens when it rejects
+    results.forEach((dino) => {
+      dinosaurs.push(dino);
+    });
+    secondDinosaurJSON().then((results2) => {//.then()-happens when resolved // .catch()-happens when it rejects
+      results2.forEach((dino) => {
+        dinosaurs.push(dino);
+      });
+      thirdDinosaurJSON().then((results3) => {//.then()-happens when resolved // .catch()-happens when it rejects
+        results3.forEach((dino) => {
+          dinosaurs.push(dino);
+        });
+        console.log("dino array", dinosaurs);
+      });
+    });
+  }).catch((error) => {
+    console.log(error);
+  });
 };
 
 const getDinosaurs = () => {
