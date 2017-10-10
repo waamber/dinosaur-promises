2ab25916ab1f04ffbd4fd6a1e921875da38b4cf5
Waamber <amberastuart@aol.com>
2017-10-09 21:09:52 -0500

    correct promise

--- javascripts/data.js
+++ javascripts/data.js
@@ -2,27 +2,6 @@
 const dom = require('./dom');
 let dinosaurs = [];
 
-
-//OLD WAY OF MULTIPLE JSON GETTER
-// const dinoGetter = () => {
-//   $.ajax('./data/dinosaurs.json').done((data1) => {
-//     data1.dinosaurs1.forEach((dino) => { //loops through dinoData and puts each dino object in array into dinosaurs array at the top
-//       dinosaurs.push(dino);
-//     });
-//   });
-//   $.ajax('./data/dinosaurs2.json').done((data2) => {
-//     data2.dinosaurs2.forEach((dino) => {
-//       dinosaurs.push(dino);
-//     });
-//   });
-//   $.ajax('./data/dinosaurs3.json').done((data3) => {
-//     data3.dinosaurs3.forEach((dino) => {
-//       dinosaurs.push(dino);
-//     });
-//     console.log(dinosaurs);
-//   });
-// };
-
 const initializer = () => {
   // dom.domString({ name: 'T-Rex' });
   dinoGetter();
@@ -58,24 +37,23 @@ const thirdDinosaurJSON = () => {
   });
 };
 
+//CORRECT PROMISES!
 const dinoGetter = () => {
   firstDinosaurJSON().then((results) => {//.then()-happens when resolved // .catch()-happens when it rejects
     results.forEach((dino) => {
       dinosaurs.push(dino);
     });
-    secondDinosaurJSON().then((results2) => {//.then()-happens when resolved // .catch()-happens when it rejects
-      results2.forEach((dino) => {
-        dinosaurs.push(dino);
-      });
-      thirdDinosaurJSON().then((results3) => {//.then()-happens when resolved // .catch()-happens when it rejects
-        results3.forEach((dino) => {
-          dinosaurs.push(dino);
-        });
-        console.log("dino array", dinosaurs);
-      });
+    return secondDinosaurJSON();
+  }).then((results2) => {
+    results2.forEach((dino) => {
+      dinosaurs.push(dino);
     });
-  }).catch((error) => {
-    console.log(error);
+    return thirdDinosaurJSON();
+  }).then((results3) => {
+    results3.forEach((dino) => {
+      dinosaurs.push(dino);
+    });
+    console.log(dinosaurs);
   });
 };
 
@@ -85,3 +63,44 @@ const getDinosaurs = () => {
 
 module.exports = { initializer, getDinosaurs };
 
+//OLD WAY OF MULTIPLE JSON GETTER
+// const dinoGetter = () => {
+//   $.ajax('./data/dinosaurs.json').done((data1) => {
+//     data1.dinosaurs1.forEach((dino) => { //loops through dinoData and puts each dino object in array into dinosaurs array at the top
+//       dinosaurs.push(dino);
+//     });
+//   });
+//   $.ajax('./data/dinosaurs2.json').done((data2) => {
+//     data2.dinosaurs2.forEach((dino) => {
+//       dinosaurs.push(dino);
+//     });
+//   });
+//   $.ajax('./data/dinosaurs3.json').done((data3) => {
+//     data3.dinosaurs3.forEach((dino) => {
+//       dinosaurs.push(dino);
+//     });
+//     console.log(dinosaurs);
+//   });
+// };
+
+//WRONG WAY
+// const dinoGetter = () => {
+//   firstDinosaurJSON().then((results) => {//.then()-happens when resolved // .catch()-happens when it rejects
+//     results.forEach((dino) => {
+//       dinosaurs.push(dino);
+//     });
+//     secondDinosaurJSON().then((results2) => {//.then()-happens when resolved // .catch()-happens when it rejects
+//       results2.forEach((dino) => {
+//         dinosaurs.push(dino);
+//       });
+//       thirdDinosaurJSON().then((results3) => {//.then()-happens when resolved // .catch()-happens when it rejects
+//         results3.forEach((dino) => {
+//           dinosaurs.push(dino);
+//         });
+//         console.log("dino array", dinosaurs);
+//       });
+//     });
+//   }).catch((error) => {
+//     console.log(error);
+//   });
+// };
